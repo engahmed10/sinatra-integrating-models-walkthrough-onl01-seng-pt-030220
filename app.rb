87d7ec5257ed422421b5d190ec3c0a91d660ace4
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-15 17:15:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,8 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    @text_from_user = params[:user_text].downcase
+    @text_from_user = params[:user_text]
+    @text_from_user=@text_from_user.downcase
     erb :results
   end
 end
